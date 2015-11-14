a0818a0cca93e60d95197e78c219ddd5a01d2422
Shane <haroboy876@gmail.com>
2015-11-13 23:43:40 -0800

    refactor save_to_drive to be more explicit

--- src/lib/save_to_drive.py
+++ src/lib/save_to_drive.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-#channel/channel_year_month_day.txt
+# channel/channel_year_month_day.txt
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
@@ -10,7 +10,20 @@ from oauth2client.client import SignedJwtAssertionCredentials
 import time
 import os
 
-def save_file_to_drive(log, channel, data): # log = filename to save as, data = the content (messages to save)
+
+def get_log_files():
+    log_files = []
+    date = time.strftime('%Y_%m_%d', time.gmtime())
+    folder = 'src/logs/{0}/'.format(date)
+    for log in os.listdir(folder):
+        if log.endswith(".txt"):
+            log_files.append("{0}{1}".format(folder, log))
+    print log_files
+    return folder, log_files
+
+
+def save_file_to_drive(log, channel, data):  # log = filename to save as, data
+    # = the content (messages to save)
     """Gets valid user credentials from storage.
     If nothing has been stored, or if the stored credentials are invalid,
     the OAuth2 flow is completed to obtain the new credentials.
@@ -40,36 +53,24 @@ def save_file_to_drive(log, channel, data): # log = filename to save as, data = 
     title = '{0}_{1}.txt'.format(channel, time.strftime('%Y_%m_%d'))
     log = drive.CreateFile({'title': title})
     log.SetContentString('Temporary')
-    log.Upload(param={'convert': True}) # Files.insert()
-
+    log.Upload(param={'convert': True})  # Files.insert()
     log['title'] = title  # Change title of the file
-    log.Upload(param={'convert': True}) # Files.patch()
+    log.Upload(param={'convert': True})  # Files.patch()
 
     content = log.GetContentString()  # 'Hello'
     log.SetContentString(data)
-    log.Upload(param={'convert': True}) # Files.update()
+    log.Upload(param={'convert': True})  # Files.update()
 
 
-def get_log_contents(folder, date, log_files):
+def save_logs_to_drive():
+    folder, log_files = get_log_files()
     for log in log_files:
-    #for log in os.listdir(folder):
         channel = log.rstrip(".txt").split("/")[3]
         filename = channel + ".txt"
         print channel
         with open("{0}{1}".format(folder, filename), 'r') as f:
             data = f.read()
             save_file_to_drive(log, channel, data)
-
-
-def get_log_files():
-    log_files = []
-    date = time.strftime('%Y_%m_%d', time.gmtime())
-    folder = 'src/logs/{0}/'.format(date)
-    for log in os.listdir(folder):
-        if log.endswith(".txt"):
-            log_files.append("{0}{1}".format(folder, log))
-    print log_files
-    get_log_contents(folder, date, log_files)
 
 
 def cron(channel):  # todo remove this arg requirement.
@@ -80,10 +81,11 @@ def cron(channel):  # todo remove this arg requirement.
         print "Date Not Matched"
         previous_date = current_date
         print "Saving files (by channel):"
-        return get_log_files()
+        return save_logs_to_drive()
     else:
         print "previous_date:", previous_date, "current_date:", current_date
 
 
 if __name__ == "__main__":
     save_file_to_drive("TESTFILE.txt", "#fakechannel", "TESTDATA")
+    save_logs_to_drive()
